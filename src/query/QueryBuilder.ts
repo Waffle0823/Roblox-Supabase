e6518261e511e43e46261af32e4080d389c5da51
Waffle0823 <csshin9928@gmail.com>
2026-01-24 05:16:41 +0900

    feat: add CRUD method stubs to QueryBuilder and import Count type

--- src/query/QueryBuilder.ts
+++ src/query/QueryBuilder.ts
@@ -1,10 +1,58 @@
-import { Headers } from "../client/types/common";
-import { GenericTable } from "../client/types/common";
+import { Headers, GenericTable } from "../client/types/common";
+import { Count } from "./types/common";
 
 export default class QueryBuilder<Table extends GenericTable> {
 	constructor(
 		private url: string,
 		private headers: Headers = {},
-		private schema: string,
 	) {}
+
+	public select(
+		columns?: keyof Table["Row"] | "*",
+		options?: {
+			head?: boolean;
+			count?: Count;
+		},
+	) {}
+
+	public insert(
+		data: Table["Insert"],
+		{
+			count,
+			defaultToNull = true,
+		}: {
+			count?: Count;
+			defaultToNull?: boolean;
+		} = {},
+	) {}
+
+	public upsert(
+		data: Table["Update"],
+		{
+			onConflict,
+			ignoreDuplicates = false,
+			count,
+			defaultToNull = true,
+		}: {
+			onConflict?: string;
+			ignoreDuplicates?: boolean;
+			count?: Count;
+			defaultToNull?: boolean;
+		} = {},
+	) {}
+
+	public update(
+		data: Table["Update"],
+		{
+			count,
+		}: {
+			count?: Count;
+		} = {},
+	) {}
+
+	public delete({
+		count,
+	}: {
+		count?: Count;
+	} = {}) {}
 }
