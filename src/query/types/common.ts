4fc6fe4f65180b0b3f1ea4a17302494a130f8a45
Waffle0823 <csshin9928@gmail.com>
2026-01-24 16:49:20 +0900

    feat: add Columns type for flexible column selection in queries

--- src/query/types/common.ts
+++ src/query/types/common.ts
@@ -1,2 +1,5 @@
+import { GenericTable } from "../../client/types/common";
+
 export type Returning = "minimal" | "representation";
 export type Count = "exact" | "planned" | "estimated";
+export type Columns<Table extends GenericTable> = keyof Table["Row"] | (keyof Table["Row"])[] | "*";
